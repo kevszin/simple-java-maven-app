a4f5c4596ceacabed75f199059517d2829bf354f
kevszin <58190171+kevszin@users.noreply.github.com>
2023-09-07 13:08:33 +0200

    Updated.v2 deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -22,5 +22,7 @@ set +x
 
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
-set -xrw
+set +x
+set +r
+set +w
 java -jar target/${NAME}-${VERSION}.jar
